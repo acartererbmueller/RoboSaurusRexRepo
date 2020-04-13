2d3446faf301a1c1f1e614166aa2ce99bd7c6bb3
unknown <carter53202@gmail.com>
2020-04-13 09:22:37 -0500

    Updated all classes and put them in their own tab

--- Robot Vs Dinosaurs/Program.cs
+++ Robot Vs Dinosaurs/Program.cs
@@ -7,9 +7,51 @@ using System.Threading.Tasks;
 namespace Robot_Vs_Dinosaurs
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            Battlefield battlefield = new Battlefield();
+            battlefield.RunBattle();
         }
     }
+
+    //Make these other classes have thier own class files
+
+    public class Dinosaur
+    {
+        string dinosaurType;
+        int levelOfDinosaurHealth;
+
+           
+    
+    
+    
+    }
+
+    public class Fleet
+    {
+        //member variable is a list of robots
+
+
+
+    }
+ 
+    public class Herd
+    {
+        //mv is a list of dinos
+
+
+
+    }
+
+    public class Weapon
+    {
+
+
+
+
+    }
+
+ 
+
 }
